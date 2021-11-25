22d8ea58b8400aa7c3031c582de76576aa1a7cb0
devBernardo-Mendes <devbernardomr@gmail.com>
2021-11-25 16:35:32 -0300

    feat: :sparkles: Create new component layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// style
+import { Container } from "./styles";
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <h1>Layout</h1>
+    </Container>
+  );
+};
+
+export default Layout;
