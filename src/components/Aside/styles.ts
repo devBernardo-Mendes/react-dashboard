198ae261973d839698e4c4644aadc5bb35b5066b
devBernardo-Mendes <devbernardomr@gmail.com>
2021-11-29 16:56:07 -0300

    feat: :sparkles: Add style for title the aplicattion

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   grid-area: AS;
 
-  background-color: ${props => props.theme.colors.secondary};
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.secondary};
 `;
