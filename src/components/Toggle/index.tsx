b48b7e30966e314cbed963587f5b8c57ceccce45
devBernardo-Mendes <devbernardomr@gmail.com>
2021-11-30 16:29:11 -0300

    feat: :sparkles: Create component Toggle

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// styles
+import { Container, ToggleLabel, ToggleSelector } from "./style";
+
+const Toggle: React.FC = () => {
+  return (
+    <Container>
+      <ToggleLabel>Ligth</ToggleLabel>;
+      <ToggleSelector
+        checked
+        uncheckedIcon={false}
+        checkedIcon={false}
+        onChange={() => {
+          console.log("Mudou");
+        }}
+      />
+      <ToggleLabel>Dark</ToggleLabel>;
+    </Container>
+  );
+};
+
+export default Toggle;
