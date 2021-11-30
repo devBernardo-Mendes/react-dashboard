8ed6d14724d54489a293365a53d2d6a4e4373c34
devBernardo-Mendes <devbernardomr@gmail.com>
2021-11-30 15:37:41 -0300

    feat: :sparkles: Create styled and component for constructor main header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+// utils
+import emojis from "../../utils/emojis";
 
 // style
-import { Container } from "./styles";
+import { Container, Profile, Welcome, UserName } from "./styles";
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji} </Welcome>
+        <UserName>Bernardo Reis</UserName>
+      </Profile>
     </Container>
   );
 };
