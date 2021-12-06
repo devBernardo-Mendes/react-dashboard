b4b569dad904b2568225aa3982b17b93a389ec0b
devBernardo-Mendes <devbernardomr@gmail.com>
2021-12-06 19:39:58 -0300

    feat: :sparkles: Create component ContentHeader

---
+++ src/components/ContentHeader/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// style
+import { Container } from "./styles";
+
+const ContentHeader: React.FC = () => {
+  return (
+    <Container>
+      <h1>Content</h1>
+    </Container>
+  );
+};
+
+export default ContentHeader;
