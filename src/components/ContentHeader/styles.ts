f6c6bb467bf3e5844555cad043003f816f3eb9b9
devBernardo-Mendes <devbernardomr@gmail.com>
2021-12-20 20:53:04 -0300

    feat: :sparkles: Pass typping for lineColor

--- src/components/ContentHeader/styles.ts
+++ src/components/ContentHeader/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { ITitleContainerProps } from "./types";
+
 export const Container = styled.div`
     width: 100%;
 
@@ -8,7 +10,7 @@ export const Container = styled.div`
 
     margin-bottom: 25px;
 `;
-export const TitleContainer = styled.div`
+export const TitleContainer = styled.div<ITitleContainerProps> `
     > h1 {
         color: ${props => props.theme.colors.white};
 
@@ -16,7 +18,7 @@ export const TitleContainer = styled.div`
          content: "";
          display: block;
          width: 55px;
-         border-bottom: 10px solid ${props => props.theme.colors.warning};
+         border-bottom: 10px solid ${props => props.lineColor};
      }
     }
 `;
---
+++ src/components/ContentHeader/types.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export interface IContentHeaderProps {
+    title: string;
+    lineColor: string;
+    children: ReactNode;
+} 
+export interface ITitleContainerProps {
+    lineColor: string;
+}
