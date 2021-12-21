aa3a81f61b4bb1aa44fb32671ecae18c43a4414e
devBernardo-Mendes <devbernardomr@gmail.com>
2021-12-21 12:01:52 -0300

    feat: :sparkles: Render and aplic props in the page list

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -6,12 +6,34 @@ import React from "react";
 // -------------------------------------------------
 // styles
 // -------------------------------------------------
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
+// -------------------------------------------------
+// components
+// -------------------------------------------------
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
+
+// -------------------------------------------------
+// utils
+// -------------------------------------------------
+import { options } from "../../utils/utils";
 
 const List: React.FC = () => {
   return (
     <Container>
-      <h1>List</h1>;
+      <ContentHeader title="Saídas" lineColor="#E44C4E">
+        <SelectInput option={options} />
+      </ContentHeader>
+      <Content>
+        <HistoryFinanceCard
+          cardColor="#313862"
+          tagColor="#E44C4E"
+          title="Conta de Luz"
+          subtitle="27/08/2021"
+          amount="R$ 320,98"
+        />
+      </Content>
     </Container>
   );
 };
