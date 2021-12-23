2bbb1963455698822fb0ca0656332443ac89a3cd
devBernardo-Mendes <devbernardomr@gmail.com>
2021-12-22 22:37:22 -0300

    feat: :sparkles: Add new dados for input

---
+++ src/utils/mock.ts
@@ -0,0 +1,11 @@
+export const months = [
+  { value: 7, label: "Julho" },
+  { value: 8, label: "Agosto" },
+  { value: 9, label: "Setembro" },
+];
+
+export const years = [
+  { value: 2022, label: 2022 },
+  { value: 2020, label: 2020 },
+  { value: 2021, label: 2021 },
+];
