fa1e9eeb2116545ca3d15baec88e7787c2d977df
devBernardo-Mendes <devbernardomr@gmail.com>
2021-12-25 20:45:19 -0300

    feat: :sparkles: Add styles for classname and add hover and after for visual aplicattion

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -13,5 +13,25 @@ export const Filters = styled.div`
   justify-content: center;
 
   .tag-filter {
+    font-size: 18px;
+    font-weight: 500;
+    background: none;
+    color: ${(props) => props.theme.colors.white};
+
+    margin: 0 10px;
+
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 0.7s;
+    }
+
+    &::after {
+      content: "";
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+    }
   }
 `;
