29dca20b9f9cc1c0b4834f00ddc919f9557d7f6f
devBernardo-Mendes <devbernardomr@gmail.com>
2022-01-06 23:01:38 -0300

    fix: :bug: Remove mensage terminal

--- src/components/SelectInput/index.tsx
+++ src/components/SelectInput/index.tsx
@@ -11,14 +11,16 @@ import { Container } from "./styles";
 // -------------------------------------------------
 // types
 // -------------------------------------------------
-import {ISelectIntupProps} from "../types";
+import { ISelectIntupProps } from "../types";
 
 const SelectInput: React.FC<ISelectIntupProps> = ({ option }) => {
   return (
     <Container>
       <select name="" id="">
         {option.map((option) => (
-          <option value={option.value}>{option.label}</option>
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
         ))}
       </select>
     </Container>
