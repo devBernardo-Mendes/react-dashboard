a76740af7284de72162c287796f277c2c86ff3cb
devBernardo-Mendes <devbernardomr@gmail.com>
2022-01-06 22:58:57 -0300

    feat: :sparkles: Creted component App for render routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+// -------------------------------------------------
+// packages
+// -------------------------------------------------
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// -------------------------------------------------
+// components
+// -------------------------------------------------
+import App from "./app.routes";
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+export default Routes;
