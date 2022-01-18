24fc27af62a79ac8c32c040e55a7860d4e51790a
devBernardo-Mendes <devbernardomr@gmail.com>
2022-01-17 21:29:20 -0300

    fix: :bug: Fix project

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,46 +2,21 @@
 // packages
 // -------------------------------------------------
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 // -------------------------------------------------
 // components
 // -------------------------------------------------
-import List from "../pages/List";
 import Dashboard from "../pages/Dashboard";
+import List from "../pages/List";
 import Layout from "../components/Layout";
 
 const AppRoutes: React.FC = () => (
   <Layout>
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route
-        path="/list/entry-balance"
-        element={
-          <List
-            match={{
-              params: {
-                type: "Entradas",
-                lineColor: "#F7931B",
-              },
-            }}
-          />
-        }
-      />
-      <Route
-        path="/list/exit-balance"
-        element={
-          <List
-            match={{
-              params: {
-                type: "Saídas",
-                lineColor: "#E44C4E",
-              },
-            }}
-          />
-        }
-      />
-    </Routes>
+    <Switch>
+      <Route path="/dashboard" exact component={Dashboard}></Route>
+      <Route path="/list/:type" exact component={List}></Route>
+    </Switch>
   </Layout>
 );
 
