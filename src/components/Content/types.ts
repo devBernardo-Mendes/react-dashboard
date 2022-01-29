6b5f90f7c8cba3cc5c5a539323c1e0e52d40c6df
devBernardo-Mendes <devbernardomr@gmail.com>
2022-01-28 21:49:31 -0300

    refactor: :recycle: Intrucer types

---
+++ src/components/Content/types.ts
@@ -0,0 +1,10 @@
+// -------------------------------------------------
+// packages
+// -------------------------------------------------
+import { ReactNode } from "react";
+
+export interface IContentHeaderProps {
+  title: string;
+  lineColor: string;
+  children: ReactNode;
+}
