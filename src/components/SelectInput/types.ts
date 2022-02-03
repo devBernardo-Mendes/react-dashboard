a9c027091424b59d142faa28b40d2149a5774e91
devBernardo-Mendes <devbernardomr@gmail.com>
2022-02-03 19:00:20 -0300

    feat: :sparkles: Add new type

--- src/components/SelectInput/types.ts
+++ src/components/SelectInput/types.ts
@@ -1,7 +1,8 @@
 export interface ISelectIntupProps {
-    option: {
-      value: string | number;
-      label: string | number;
-    }[];
-    onChange(event: React.ChangeEvent<HTMLSelectElement>): void | undefined;
-  }
+  option: {
+    value: string | number;
+    label: string | number;
+  }[];
+  onChange(event: React.ChangeEvent<HTMLSelectElement>): void | undefined;
+  defaultValue?: string | number;
+}
