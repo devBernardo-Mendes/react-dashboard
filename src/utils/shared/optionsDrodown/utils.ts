51b39a890592b456bd990fb91d7253caced55b1a
devBernardo-Mendes <devbernardomr@gmail.com>
2022-02-03 19:01:31 -0300

    feat: :sparkles: Add new year and month

--- src/utils/shared/optionsDrodown/utils.ts
+++ src/utils/shared/optionsDrodown/utils.ts
@@ -1,11 +1,13 @@
 export const months = [
-  { value: 7, label: "Julho" },
+  { value: 1, label: "Janeiro" },
+  { value: 5, label: "Maio" },
+  { value: 6, label: "Junho" },
   { value: 8, label: "Agosto" },
   { value: 9, label: "Setembro" },
 ];
 
 export const years = [
   { value: 2022, label: 2022 },
-  { value: 2020, label: 2020 },
   { value: 2021, label: 2021 },
+  { value: 2020, label: 2020 },
 ];
