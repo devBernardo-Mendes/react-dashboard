41bfaf5279ca125fe9d67f59f054b7d75b444bd9
devBernardo-Mendes <devbernardomr@gmail.com>
2022-02-08 16:08:25 -0300

    fix: :bug: Fix color of button

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -33,13 +33,13 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
   }
   .tag-filter-eventual::after {
     content: "";
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
   }
 `;
